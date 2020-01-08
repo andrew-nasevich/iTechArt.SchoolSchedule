c7d03cb979740b8ff983e64a6e6d901addb3a9c7
Andrew <nasevichandrey@gmail.com>
2020-01-08 12:29:54 +0300

    logging was configured

--- src/iTechArt.SchoolSchedule/Global.asax.cs
+++ src/iTechArt.SchoolSchedule/Global.asax.cs
@@ -1,18 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Unity;
+using iTechArt.Common.Logging;
+using Unity.Lifetime;
 
 namespace iTechArt.SchoolSchedule
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
+        private static readonly IUnityContainer _unityContainer = new UnityContainer();
+
+
         protected void Application_Start()
         {
+            FillContainer();
+
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void FillContainer()
+        {
+            _unityContainer.RegisterType<ILogger, Logger>(new ContainerControlledLifetimeManager());
         }
     }
 }
