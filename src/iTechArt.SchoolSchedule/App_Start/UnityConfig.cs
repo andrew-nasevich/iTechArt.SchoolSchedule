3445a0f8f64535c5fbf8aee5f31fea29455a1de8
Andrew <nasevichandrey@gmail.com>
2020-01-09 14:04:03 +0300

    ContextLogger was given a particular logger; Code styles were used

--- src/iTechArt.SchoolSchedule/App_Start/UnityConfig.cs
+++ src/iTechArt.SchoolSchedule/App_Start/UnityConfig.cs
@@ -6,18 +6,12 @@ namespace iTechArt.SchoolSchedule
 {
     public static class UnityConfig
     {
-        private static readonly IUnityContainer _unityContainer = new UnityContainer();
+        public static IUnityContainer UnityContainer { get; } = new UnityContainer();
 
 
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
--- src/iTechArt.SchoolSchedule/Global.asax.cs
+++ src/iTechArt.SchoolSchedule/Global.asax.cs
@@ -1,19 +1,27 @@
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using Unity;
 using iTechArt.Common.Logging;
-using Unity.Lifetime;
+using Unity;
+using Unity.Resolution;
 
 namespace iTechArt.SchoolSchedule
 {
     public class MvcApplication : HttpApplication
-    { 
+    {
         protected void Application_Start()
-        { 
+        {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             UnityConfig.RegisterComponents();
+            ConfigureLogging();
+        }
+
+
+        private void ConfigureLogging()
+        {
+            LoggerContext.Current = UnityConfig.UnityContainer.Resolve<Logger>(
+                new ParameterOverride("name", "iTechArt.SchoolSchedule logger"));
         }
     }
 }
