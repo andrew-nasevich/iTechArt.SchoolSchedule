7d7c7112af15e3df50219dcda630d7f1349605a6
Andrew <nasevichandrey@gmail.com>
2020-01-27 15:01:38 +0300

    test code was removed

--- src/iTechArt.SchoolSchedule/Controllers/HomeController.cs
+++ src/iTechArt.SchoolSchedule/Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace iTechArt.SchoolSchedule.Controllers
         // GET: Home
         public string Index()
         {
-            var context = new SchoolScheduleContext();
-            var uow = new SchoolScheduleUnitOfWork(context);
-            var rep = uow.GetRepository<Classroom>();
-            var c = new Classroom {Id = 1, Name = "1"};
-            rep.Add(c);
-            var people = rep.GetAllAsync().Result;
             return "Hello, World!";
         }
     }
